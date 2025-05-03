0c63007c77321b474f236331975e700e04c64f40
velan-07 <velan00777@gmail.com>
2025-05-03 11:35:40 +0530

    Folder Structure Created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+	name = "Medical Chatbot",
+	version = "0.0.0",
+	author = "Velan",
+	author_email = "velan00777@gmail.com",
+	packages = find_packages(),
+	install_requires = []
+)
